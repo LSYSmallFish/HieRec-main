2de6458c43428cc31c443752b8a4463ff6fae967
SmallFish <332547960@qq.com>
2022-04-26 20:20:50 +0800

    GPU使用设置

--- Code/Main.py
+++ Code/Main.py
@@ -6,6 +6,9 @@ import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
 
 config = tf.ConfigProto()
+
+config.gpu_options.per_process_gpu_memory_fraction = 0.93
+config.gpu_options.allocator_type = 'BFC'
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
